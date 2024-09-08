26539892a1d6d73af68e586de31485841b87bac9
Duc Hoang <hpmduc3010@gmail.com>
2024-09-08 15:18:30 +0700

    Refactor script exportPage, make it clean

--- parse-xml/exportPage.js
+++ parse-xml/exportPage.js
@@ -5,6 +5,14 @@ const moment = require('moment');
 const helpers = require('./helpers');
 const cwd = process.cwd();
 
+const CONFIG = {
+    SOURCE: 'ignore/20240905_library_SamsoniteSharedLibrary.xml',
+    PAGE_ID: [
+        'jp-homepage-revamp'
+    ],
+    EXPORT_PATTERN: 'export-page'
+};
+
 function createContentMapByID(xmlObj) {
     const objWithContentIDAsKey = {};
     const contentArray = xmlObj && xmlObj.library && xmlObj.library.content;
@@ -27,33 +35,33 @@ function getContentLinkIDs(content) {
     return [];
 }
 
-async function main() {
-    const relativeFilePath = 'ignore/20240905_library_SamsoniteSharedLibrary.xml';
-    const pageIDs = [
-        'jp-homepage-revamp',
-        'tw-homepage-revamp'
-    ];
-    const exportFilePattern = 'export-page';
-    const xmlPath = path.join(cwd, relativeFilePath);
-    const fullXmlObj = await helpers.xmlToJSON(xmlPath);
-
-    /** Extract related layout, region, component IDs of the page to one array */
-    const contentMapByID = createContentMapByID(fullXmlObj);
-    let relatedContentIDs = [].concat(pageIDs);
+/**
+ * Extract related layout, region, component IDs of the page to one array
+ * @param {Object} xmlObj - Source XML object
+ * @returns {Array} - Content array
+ */
+function extractRelatedContent(xmlObj) {
+    const contentMapByID = createContentMapByID(xmlObj);
+    let relatedContentIDs = [].concat(CONFIG.PAGE_ID);
 
     for (let i = 0; i < relatedContentIDs.length; i++) {
         const contentID = relatedContentIDs[i];
         const content = contentMapByID[contentID];
         const contentLinkIDs = getContentLinkIDs(content);
-        relatedContentIDs = [...new Set([...relatedContentIDs ,...contentLinkIDs])];
+        relatedContentIDs = [...new Set([...relatedContentIDs, ...contentLinkIDs])];
     }
 
-    const relatedContents = relatedContentIDs.map(contentID => {
+    return relatedContentIDs.map(contentID => {
         return contentMapByID[contentID];
     }).filter(content => {
         return !!content;
     });
-    /** */
+}
+
+async function main() {
+    const xmlPath = path.join(cwd, CONFIG.SOURCE);
+    const fullXmlObj = await helpers.xmlToJSON(xmlPath);
+    const relatedContents = extractRelatedContent(fullXmlObj);
 
     console.log(`Related content amount: ${relatedContents.length}`);
 
@@ -62,7 +70,7 @@ async function main() {
 
     const finalXml = helpers.buildXML(fullXmlObj);
     const date = moment().format('YYYYMMDDkkmmss');
-    helpers.exportXml(xmlPath, finalXml, `${date}_${exportFilePattern}.xml`);
+    helpers.exportXml(xmlPath, finalXml, `${date}_${CONFIG.EXPORT_PATTERN}.xml`);
 }
 
 main();
