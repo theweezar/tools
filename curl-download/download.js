f4cc0377ada71b069153fbad80a2e7186e84e7c4
Duc Hoang <hpmduc3010@gmail.com>
2024-10-13 23:55:02 +0700

    Update script to download image from Patreon

--- curl-download/download.js
+++ curl-download/download.js
@@ -4,16 +4,17 @@ const shell = require('shelljs');
 const path = require('path');
 const fs = require('fs');
 const cmd = require('./helpers/cmd');
+const { default: axios } = require('axios');
 const cwd = process.cwd();
 
 const config = (() => {
-    const configJson = require(path.join(cwd, './download.config.json'));
+    const configJson = require(path.join(cwd, './curl-download/download.config.json'));
 
     const parseEntryModel = () => {
-        return configJson.entries.map(url => {
-            let url = new URL(url);
+        return configJson.entries.map(entry => {
+            let url = new URL(entry);
             return {
-                fileName: url.split('/').pop(),
+                fileName: entry.split('/').pop(),
                 hostName: url.hostname,
                 relativePath: url.pathname.substring(0, url.pathname.lastIndexOf('/')),
                 fullPath: url
@@ -23,7 +24,8 @@ const config = (() => {
 
     return {
         outputPath: path.join(cwd, configJson.output),
-        entries: parseEntryModel()
+        entries: parseEntryModel(),
+        type: configJson.type || 'direct'
     };
 })();
 
@@ -45,23 +47,56 @@ function getFilesInOuputDir() {
     return files.map(file => file);
 }
 
+function executePatreonDownloader() {
+    config.entries.forEach(async (entryModel) => {
+        let res = await axios.get(entryModel.fullPath, {
+            headers: {
+                cookie: "patreon_device_id=4a042cb4-4511-40b7-8afb-ef2892a88f26; patreon_location_country_code=VN; patreon_locale_code=en-US; __ssid=3bc3345b0b963641c34b8fb37754070; g_state={\"i_l\":0}; session_id=mgycZOhSZBFSmVOl5j5auyLG2_ciax1x1dr2LY_dpT8; __cf_bm=XdUKmnsE4KhDY9bRKD3BRByDIyPSN1.cx1KyqZFGaZw-1728835611-1.0.1.1-MyjOLLMY1aOG5P.zqavXTZWP1oqUoCSdhdcTJYer2QT1pDLuYBSOoM3ci2lznhzOzX86UfawDfEN4tpJBpvIQ215Re_YXNpu0FzQT7idD5k; analytics_session_id=97c88742-0e9b-4188-ad1a-38952b888fd2"
+            }
+        });
+        
+        let exposedHeader = res.request._header;
+        let split = typeof exposedHeader === 'string' ? exposedHeader.split(/GET ([^]*) HTTP\/1.1(?:[^]*)/g) : [];
+        let endpoint = split.length >= 2 ? split[1] : '';
+
+        console.log(res);
+
+        if (endpoint !== '') {
+            let baseUrl = new URL('https://c10.patreonusercontent.com');
+            let mediaUrl = new URL(endpoint, baseUrl);
+            // console.log(mediaUrl);
+        }
+    });
+}
+
 function execute() {
     mkDirIfNotExist();
 
     let fileNames = getFilesInOuputDir();
     let count = 0;
 
-    config.entries.forEach(entryModel => {
-        if (!fileNames.includes(entryModel.fileName)) {
-            cmd.exeCurl(
-                entryModel.fullPath,
-                path.join(config.outputPath, entryModel.fileName)
-            );
-            count++;
-        }
-    });
+    switch (config.type) {
+        case 'direct':
+            config.entries.forEach(entryModel => {
+                if (!fileNames.includes(entryModel.fileName)) {
+                    cmd.exeCurl(
+                        entryModel.fullPath,
+                        path.join(config.outputPath, entryModel.fileName)
+                    );
+                    count++;
+                }
+            });
+            break;
 
-    console.log(`Downloaded ${count} files`);
+        case 'patreon':
+            executePatreonDownloader();
+            break;
+
+        default:
+            break;
+    }
+
+    // console.log(`Downloaded ${count} files`);
 }
 
 execute();
