74af6f08e800373c40609bc5c35d9253602e0ef2
Duc Hoang <hpmduc3010@gmail.com>
2024-10-16 00:17:08 +0700

    Update script to download image from Patreon

--- curl-download/client.js
+++ curl-download/client.js
@@ -7,4 +7,13 @@
     console.log(imgList);
 })();
 
-
+// Patreon
+(() => {
+    let linkEls = document.querySelectorAll('[data-tag="post-attachments"] a[data-tag="post-attachment-link"]');
+    let links = [];
+    let url = new URL(window.location.href);
+    linkEls.forEach(el => {
+        links.push(`${url.protocol}//${url.hostname}${el.getAttribute('href')}`);
+    });
+    console.log(links);
+})();
