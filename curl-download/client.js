6ac545d094c079764a5648e5e8e7bc764c484c35
Duc Hoang <hpmduc3010@gmail.com>
2025-01-11 13:45:26 +0700

    Add option to download patreon user content directly

--- curl-download/client.js
+++ curl-download/client.js
@@ -17,3 +17,22 @@
     });
     console.log(links);
 })();
+
+// Patreon post link
+(() => {
+    const getProp = (t, r) => { if (!t || !r) return null; let e = t, f = r.split("."); if (e) for (; f.length;) { if (e = e[f.shift()], !e) break } return e };
+    const nextDataJSON = document.querySelector('script[id="__NEXT_DATA__"]').innerText;
+    const nextData = JSON.parse(nextDataJSON);
+
+    if (nextData) {
+        let included = getProp(nextData, 'props.pageProps.bootstrapEnvelope.pageBootstrap.post.included');
+
+        if (Array.isArray(included)) {
+            console.log(
+                included.map(el => {
+                    return getProp(el, 'attributes.image_urls.original');
+                }).filter(link => !!link)
+            );
+        }
+    }
+})();
