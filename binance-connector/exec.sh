0cd1c6ca8098fb69f668c160b9c6d4812505a092
Duc Hoang <hpmduc3010@gmail.com>
2025-01-20 00:00:03 +0700

    Move binance-connector to another repository

--- binance-connector/exec.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-node ./js/fetch.js -p "ignore" -s "BTC" -i "1h" -f 1 -D
-# python ./py/processor.py ./ignore
-python ./py/main.py ./ignore
-# python ./py/deep_learning_model.py ./ignore
